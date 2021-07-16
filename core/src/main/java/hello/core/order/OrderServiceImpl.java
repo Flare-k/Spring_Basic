5a251282fda86644764ccab04747c4fd83666d33
rokkyw <rokkyw@naver.com>
2021-07-16 16:38:57 +0900

    주문 서비스 & 할인 서비스 구현

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // 주문 생성
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원정보 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인율 조회 (member가 아닌 Grade만 넘기도록 설계해도 된다)
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
