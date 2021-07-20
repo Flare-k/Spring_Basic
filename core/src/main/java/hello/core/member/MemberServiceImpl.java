d190c2a910d548ca0c15f188074a0d569a99c0c4
rokkyw <rokkyw@naver.com>
2021-07-20 22:16:32 +0900

    DIP 해결방안 -> AppConfig의 역할

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;    // 생성자 할당을 이용하므로 final을 써준다.
+
+    // 생성자 주입: 구현체가 아닌 외부에 있는 AppConfig가 DI를 담당한다.
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
