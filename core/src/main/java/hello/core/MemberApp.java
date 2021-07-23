9be6e4007f5ec015c1dea45dce379a274e40ada2
rokkyw <rokkyw@naver.com>
2021-07-23 14:52:50 +0900

    MemberApp, OrderApp: Java -> Spring

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         //MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 스프링 컨테이너로부터 key-value 방식처럼 이름("memberService")을 입력해 꺼내옵니다. getBean(메소드 명, 타입)
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
